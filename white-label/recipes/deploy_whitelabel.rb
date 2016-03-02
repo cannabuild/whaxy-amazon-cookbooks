1c100e2de0ceb7f33a2dee1819f4121686dfc859
Micah Davidson <micah@mond.us>
2016-03-02 12:47:28 -0700

    First pass at new white-label nginx recipe.

---
+++ white-label/recipes/deploy_whitelabel.rb
@@ -0,0 +1,13 @@
+include_recipe 'deploy'
+
+node[:deploy].each do |application, deploy|
+  template 'nginx site' do
+    path ::File.join('/etc/nginx/sites-enabled/', deploy'whaxy.com')
+    source 'whitelabel-nginx.erb'
+    owner 'root'
+    group 'root'
+    mode 0644
+    backup false
+    variables(:webapp_name => application)
+  end
+end
