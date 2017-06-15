49cf8285a179e9bb425a8061676bc8e396284ebc
Sebastian Vela <seb@massroots.com>
2017-06-15 17:04:35 -0600

    test more changes to metadata

--- nodejs/metadata.rb
+++ nodejs/metadata.rb
@@ -1,6 +1,6 @@
 depends 'build-essential'
 depends 'ark', '>= 2.0.2'
-depends 'compat_resource', '>= 12.16'
+depends 'compat_resource'
 
 %w(debian ubuntu centos redhat scientific oracle amazon smartos mac_os_x opensuse opensuseleap suse).each do |os|
   supports os
